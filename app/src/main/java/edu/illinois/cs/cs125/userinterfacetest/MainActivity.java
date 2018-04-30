a8605c1280b579f33b25d0f5a63a25045b99bdbc
GoldenSmearing <derektd2@illinios.edu>
2018-04-30 17:43:07 -0500

    Everything is working. Everything is great. This is the best project ever.

--- app/src/main/java/edu/illinois/cs/cs125/userinterfacetest/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/userinterfacetest/MainActivity.java
@@ -6,13 +6,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
+
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "User Interface: Main";
-    public String headlineTrue = "This";
-    public String headlineFalse = "Not this";
     public boolean topTrue = true;
     public int numCorrect = 0;
     public int totalRounds = 0;
@@ -29,9 +41,9 @@ public class MainActivity extends AppCompatActivity {
         final TextView topHeadline = findViewById(R.id.topHeadline);
         final TextView bottomHeadline = findViewById(R.id.bottomHeadline);
         topHeadline.setText("''Big Don talks with Kim Jong''");
-        topHeadline.setTextSize(35);
+        topHeadline.setTextSize(20);
         bottomHeadline.setText("''Handshake between Donald and Kim goes well''");
-        bottomHeadline.setTextSize(35);
+        bottomHeadline.setTextSize(20);
 
         /*
          * Set up handlers for each button in our UI. These run when the buttons are clicked.
@@ -57,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         bottomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
@@ -114,12 +125,97 @@ public class MainActivity extends AppCompatActivity {
         int choice = (int) (Math.random() * 2);
         if (choice == 0) {
             topTrue = true;
-            topHeadline.setText(headlineTrue);
-            bottomHeadline.setText(headlineFalse);
+            String url = "https://www.reddit.com/r/nottheonion/random.json?limit=1";
+            RequestQueue queue = Volley.newRequestQueue(this);
+            StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    JsonParser parser = new JsonParser();
+                    JsonArray result = parser.parse(response.toString()).getAsJsonArray();
+                    JsonObject article = result.get(0).getAsJsonObject();
+                    JsonObject data = article.get("data").getAsJsonObject();
+                    JsonArray children = data.get("children").getAsJsonArray();
+                    JsonObject child = children.get(0).getAsJsonObject();
+                    JsonObject data2 = child.get("data").getAsJsonObject();
+                    String title = data2.get("title").getAsString();
+                    topHeadline.setText(title);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.v("run", error.toString());
+                }
+            });
+            queue.add(stringRequest1);
+            url = "https://www.reddit.com/r/theonion/random.json?limit=1";
+            StringRequest stringRequest2 = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    JsonParser parser = new JsonParser();
+                    JsonArray result = parser.parse(response.toString()).getAsJsonArray();
+                    JsonObject article = result.get(0).getAsJsonObject();
+                    JsonObject data = article.get("data").getAsJsonObject();
+                    JsonArray children = data.get("children").getAsJsonArray();
+                    JsonObject child = children.get(0).getAsJsonObject();
+                    JsonObject data2 = child.get("data").getAsJsonObject();
+                    String title = data2.get("title").getAsString();
+                    bottomHeadline.setText(title);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.v("run", error.toString());
+                }
+            });
+            queue.add(stringRequest2);
         } else {
             topTrue = false;
-            topHeadline.setText(headlineFalse);
-            topHeadline.setText(headlineTrue);
+            String url = "https://www.reddit.com/r/theonion/random.json?limit=1";
+            RequestQueue queue = Volley.newRequestQueue(this);
+            StringRequest stringRequest1 = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    JsonParser parser = new JsonParser();
+                    JsonArray result = parser.parse(response.toString()).getAsJsonArray();
+                    JsonObject article = result.get(0).getAsJsonObject();
+                    JsonObject data = article.get("data").getAsJsonObject();
+                    JsonArray children = data.get("children").getAsJsonArray();
+                    JsonObject child = children.get(0).getAsJsonObject();
+                    JsonObject data2 = child.get("data").getAsJsonObject();
+                    String title = data2.get("title").getAsString();
+                    topHeadline.setText(title);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.v("run", error.toString());
+                }
+            });
+            queue.add(stringRequest1);
+            url = "https://www.reddit.com/r/nottheonion/random.json?limit=1";
+            StringRequest stringRequest2 = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    JsonParser parser = new JsonParser();
+                    JsonArray result = parser.parse(response.toString()).getAsJsonArray();
+                    JsonObject article = result.get(0).getAsJsonObject();
+                    JsonObject data = article.get("data").getAsJsonObject();
+                    JsonArray children = data.get("children").getAsJsonArray();
+                    JsonObject child = children.get(0).getAsJsonObject();
+                    JsonObject data2 = child.get("data").getAsJsonObject();
+                    String title = data2.get("title").getAsString();
+                    bottomHeadline.setText(title);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.v("run", error.toString());
+                }
+            });
+            queue.add(stringRequest2);
         }
     }
+
+
+
 }
